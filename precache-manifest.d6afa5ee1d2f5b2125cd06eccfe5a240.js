8fe8c540f6538bebaf5cf49097b0513f6e188c6c
Carl Dobrovic <cadobrovic@gmail.com>
2020-07-08 18:54:22 -0400

    Updates

---
+++ precache-manifest.d6afa5ee1d2f5b2125cd06eccfe5a240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8973fdf546534685019be8dda2f009db",
+    "url": "/blm2020/index.html"
+  },
+  {
+    "revision": "8cedff4f3a4b1c2969d4",
+    "url": "/blm2020/static/css/main.c27281dc.chunk.css"
+  },
+  {
+    "revision": "4415514661aab02ba9f6",
+    "url": "/blm2020/static/js/2.f9da808d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blm2020/static/js/2.f9da808d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cedff4f3a4b1c2969d4",
+    "url": "/blm2020/static/js/main.a58f9e6a.chunk.js"
+  },
+  {
+    "revision": "44805dfdf7d4109eb1de",
+    "url": "/blm2020/static/js/runtime-main.9bdba9eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blm2020/precache-manifest.97216b8a78e950449856b7a5cb0f519d.js"
+  "/blm2020/precache-manifest.d6afa5ee1d2f5b2125cd06eccfe5a240.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/blm2020/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blm2020/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
