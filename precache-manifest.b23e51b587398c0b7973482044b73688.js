c616b43dc81ed0634f54912fc1f3866f9e3abced
Carl Dobrovic <cadobrovic@gmail.com>
2020-07-08 01:23:27 -0400

    Updates

---
+++ precache-manifest.b23e51b587398c0b7973482044b73688.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "120db5bacc09c26c36ac",
+    "url": "/static/js/main.120db5ba.js"
+  },
+  {
+    "revision": "120db5bacc09c26c36ac",
+    "url": "/static/css/main.1b11db1a.css"
+  },
+  {
+    "revision": "9b628f6a639e1964802708c18214552c",
+    "url": "/index.html"
+  }
+];
